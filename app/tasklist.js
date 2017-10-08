ca485d9b6906ea1ce940c522b88d64f20e09d08e
yjkim97 <31455704+yjkim97@users.noreply.github.com>
2017-10-07 14:55:47 -1000

    Task 3: Tasklist form

--- app/tasklist.js
+++ app/tasklist.js
@@ -6,6 +6,24 @@ if (Meteor.isClient) {
       return Tasks.find({}, {sort: {createAt: -1}});
     }
   });
+
+  Template.tasks.events({
+    "submit .add-task": function(event) {
+      let name = event.target.name.value;
+      Tasks.insert({
+        name: name, createAt: new Date()
+      });
+      event.target.name.value = '';
+      return false;
+    },
+
+    "click .delete-task": function(event) {
+      if (confirm('Delete Task?')) {
+        Tasks.remove(this._id);
+      }
+      return false;
+    }
+  })
 }
 
 if (Meteor.isServer) {
